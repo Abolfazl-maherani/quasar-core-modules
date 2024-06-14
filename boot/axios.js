101b75be0e81c0acbb8856f3666816850c9ccf42
dev.Abolfazl <mahrani.prj@gmail.com>
2024-06-15 02:45:24 +0330

    Setup auth

--- boot/axios.js
+++ boot/axios.js
@@ -1,7 +1,9 @@
 import { boot } from "quasar/wrappers";
 import axios from "axios";
 import { useCoreStore } from "../stores/coreStore";
-import { Quasar } from "quasar";
+import { Notify } from "quasar";
+import { mt } from "src/modules/core/boot/i18n";
+import { useAuth } from "src/modules/core/composables/useAuth";
 
 // Be careful when using SSR for cross-request state pollution
 // due to creating a Singleton instance here;
@@ -11,7 +13,12 @@ import { Quasar } from "quasar";
 // for each client)
 
 const axiosInstance = axios.create();
-
+const alert = (message) =>
+  Notify.create({
+    message,
+    type: "negative",
+    position: "top",
+  });
 export default boot(({ app, store, ssrContext }) => {
   const coreStore = useCoreStore(store);
 
@@ -25,6 +32,46 @@ export default boot(({ app, store, ssrContext }) => {
 
       return request;
     });
+    axiosInstance.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if (process.env.CLIENT) {
+          const { data = undefined, status } = error?.response || {};
+
+          if (data && status) {
+            const { message } = data;
+            switch (status) {
+              case 500: {
+                alert(message || mt("app", "Internal server error"));
+                break;
+              }
+              case 401: {
+                const { logout } = useAuth();
+                alert(message || mt("app", "You do not have access"));
+                logout();
+                break;
+              }
+              case 400: {
+                if (data?.errors && Object.keys(data?.errors)?.length) {
+                  const firstError = Object.entries(data.errors);
+                  const messageFirstError = firstError?.[0]?.[1]?.msg;
+                  alert(messageFirstError);
+                  return;
+                }
+                alert(message || mt("app", "Your input is invalid"));
+                break;
+              }
+              default: {
+                if (message) alert(message);
+                break;
+              }
+            }
+          }
+        }
+
+        return Promise.reject(error);
+      }
+    );
   }
 
   console.log(process.env.API_URL);
--- composables/useAuth.js
+++ composables/useAuth.js
@@ -1,22 +1,46 @@
-import { computed } from "vue";
-import { useLocalStorage } from "@vueuse/core";
+import { computed, onBeforeMount } from "vue";
 import { LocalStorage } from "quasar";
+import { useAppStore } from "src/modules/app/stores/appStore";
 
 export const useAuth = () => {
-  const getUser = computed(() => LocalStorage.getItem("user-data"));
-  const getToken = computed(() => LocalStorage.getItem("access-token"));
-  const isLogin = computed(() => !!getToken.value);
-  const setUser = (user) => {
-    LocalStorage.set("user-data", user);
+  const appStore = useAppStore();
+  onBeforeMount(() => {
+    appStore.setUser(LocalStorage.getItem("user-data"));
+    appStore.setToken(LocalStorage.getItem("access-token"));
+  });
+
+  const getUser = computed(() => appStore.getUser);
+  const getToken = computed(() => appStore.getToken);
+  const isLogin = computed(() => !!(getToken.value && getUser.value));
+  const setUser = (userValue) => {
+    try {
+      LocalStorage.set("user-data", userValue);
+      appStore.setUser(userValue);
+    } catch (err) {
+      console.log(err);
+    }
   };
-  const setToken = (token) => {
-    LocalStorage.set("access-token", token);
+
+  const setToken = (tokenValue) => {
+    try {
+      LocalStorage.set("access-token", tokenValue);
+      appStore.setToken(tokenValue);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const logout = () => {
+    LocalStorage.remove("access-token");
+    LocalStorage.remove("user-data");
+    appStore.setUser(null);
+    appStore.setToken(null);
   };
   return {
     getToken,
     isLogin,
     getUser,
     setToken,
+    logout,
     setUser,
   };
 };
